f937062e855ac939c92c7eed8bfbeb907152b021
SOFIKUL SK <148935407+sofikulsk02@users.noreply.github.com>
2025-10-24 15:54:47 +0530

    initial commit

---
+++ githubWithReact/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="bg-slate-700 rounded-sm shadow-lg h-[50%] w-[50%] border border-black">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos rem
+      libero non, voluptates quisquam vitae sequi consequuntur tempore quasi,
+      ullam expedita nulla aut placeat doloremque neque aspernatur accusamus!
+      Ullam.
+    </div>
+  );
+};
+
+export default Home;
