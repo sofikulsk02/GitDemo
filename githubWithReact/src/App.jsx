3758519c6a30207cdabd1d4b19d1dfcf385d7f6b
SOFIKUL SK <148935407+sofikulsk02@users.noreply.github.com>
2025-10-24 15:59:09 +0530

    from login

--- githubWithReact/src/App.jsx
+++ githubWithReact/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Home from "./components/Home";
+import Login from "./components/Login";
 
 const App = () => {
   return (
     <div>
       <Home></Home>
+      <Login></Login>
     </div>
   );
 };
---
+++ githubWithReact/src/components/Login.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Login = () => {
+  return <div className="bg-sky-500 text-black h-11 w-10">Login</div>;
+};
+
+export default Login;
